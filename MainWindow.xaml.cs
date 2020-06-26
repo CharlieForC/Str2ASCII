79edf272960416b3f5a3dc860531b133e92f779a
CharlieForC <lypycxz@163.com>
2020-06-26 11:47:17 +0800

    第一次上传

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace str2ascii
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ASCII_Hex(string fill)
+        {
+            byte[] asciiTem = Encoding.Default.GetBytes(strbox.Text);
+            asciibox.Text = null;
+            for (int i = 0; i < asciiTem.Length; i++)
+            {
+                int asciicode = (int)(asciiTem[i]);
+                if (i == 0)
+                    asciibox.Text += fill.Substring(1);
+                else
+                    asciibox.Text += fill;
+                asciibox.Text += asciicode.ToString("X");
+                //asciibox.Text += Convert.ToString(asciicode,16);//字符串ASCIIstr2 为对应的ASCII字符串
+            }
+
+        }
+        private void stabut_Click(object sender, RoutedEventArgs e)
+        {
+            ASCII_Hex(" ");
+        }
+
+        private void strbox_enter(object sender, KeyEventArgs e)
+        {
+           // MessageBox.Show("");
+            
+            if (e.KeyboardDevice.Modifiers == ModifierKeys.Control && e.Key ==Key.Enter)
+            {
+                ASCII_Hex(",0x");
+            }
+
+            if (e.KeyboardDevice.Modifiers != ModifierKeys.Control && e.Key == Key.Enter)
+            {
+                ASCII_Hex(" ");
+            }
+        }
+    }
+}
